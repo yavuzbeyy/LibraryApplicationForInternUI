6e1763f189c667717d320a70c24da7e61764682c
Yavuz <devyavuz81@gmail.com>
2024-04-22 14:32:21 +0300

    yazarEklemeOlusturuldu

--- src/components/Screens/admin/add-author/add-author.component.ts
+++ src/components/Screens/admin/add-author/add-author.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../../Shared/services/DataService';
+import { AuthorModel } from '../../../Shared/Models/AuthorModel';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-author',
   templateUrl: './add-author.component.html',
-  styleUrl: './add-author.component.scss'
+  styleUrls: ['./add-author.component.scss']
 })
 export class AddAuthorComponent {
+  author: AuthorModel = new AuthorModel();
 
+  constructor(private dataService: DataService, private toastr: ToastrService) {}
+
+  submitForm() {
+    this.dataService.createAuthor(this.author).subscribe(
+      (response) => {
+        console.log('Author added successfully!', response);
+        this.toastr.success('Author added successfully!', 'Success');
+        // Başka bir işlem yapabilirsiniz (örneğin başka bir sayfaya yönlendirme)
+      },
+      (error) => {
+        console.error('Error adding author:', error);
+        this.toastr.error('Error adding author. Please try again.', 'Error');
+        // Hata durumunda kullanıcıya bildirim gösterilebilir
+      }
+    );
+  }
 }
